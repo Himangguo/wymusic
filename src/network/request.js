b51e0138fd778130b0a5cbe0e9fbbce1c97a536b
Himangguo <46859940+Himangguo@users.noreply.github.com>
2020-05-18 19:53:20 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -16,7 +16,7 @@ import vue from "vue";
 export function request(config) {
   // 创建axios实例
   const instance = axios.create({
-    baseURL: "http://192.168.0.105:3000",
+    baseURL: "http://127.0.0.1:3000",
     timeout: 5000,
   });
   // 请求拦截
